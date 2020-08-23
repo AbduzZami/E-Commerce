8ad6f9e77d55bcf6e9e96ed84b8b01cd0d9c0eea
AbduzZami <69592754+AbduzZami@users.noreply.github.com>
2020-08-24 00:35:02 +0600

    test

--- app/src/main/java/com/example/e_shop/User.java
+++ app/src/main/java/com/example/e_shop/User.java
@@ -4,6 +4,7 @@ public class User {
     String username;
     String phone;
     String address;
+    String imageurlext;
 
     public String getUsername() {
         return username;
@@ -29,12 +30,21 @@ public class User {
         this.address = address;
     }
 
+    public String getImageurlext() {
+        return imageurlext;
+    }
+
+    public void setImageurlext(String imageurlext) {
+        this.imageurlext = imageurlext;
+    }
+
     public User() {
     }
 
-    public User(String username, String phone, String address) {
+    public User(String username, String phone, String address,String imageurl) {
         this.username = username;
         this.phone = phone;
         this.address = address;
+        this.imageurlext = imageurl ;
     }
 }
--- app/src/main/java/com/example/e_shop/UserProfile.java
+++ app/src/main/java/com/example/e_shop/UserProfile.java
@@ -1,18 +1,28 @@
 package com.example.e_shop;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,8 +30,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
+import java.net.URI;
 import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserProfile extends AppCompatActivity {
     private EditText editTextusername,editTextaddress,editTextphoneno;
@@ -29,7 +47,13 @@ public class UserProfile extends AppCompatActivity {
     private DatabaseReference databaseReference;
     private FirebaseUser firebaseUser;
 
-
+    private static int IMAGE_REQUEST = 1 ;
+    private Uri image_url ;
+    private StorageTask storageTask;
+    private StorageReference storageReference ;
+    private CircleImageView profilepic ;
+    private String propicextension ;
+    static int picselected = 0 ;
 
     
 
@@ -44,6 +68,8 @@ public class UserProfile extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        storageReference = FirebaseStorage.getInstance().getReference("ProfilePictures");
+
         editTextusername = findViewById(R.id.editTextTextPersonName2);
         editTextaddress = findViewById(R.id.editTextTextPersonName3);
         editTextphoneno = findViewById(R.id.editTextTextPersonName4);
@@ -51,6 +77,14 @@ public class UserProfile extends AppCompatActivity {
         buttonupdate = findViewById(R.id.button6);
 
         buttonlogout = findViewById(R.id.button8);
+        profilepic = findViewById(R.id.profile_image);
+        profilepic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenImage();
+                picselected = 1 ;
+            }
+        });
 
 
         buttonlogout.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +100,7 @@ public class UserProfile extends AppCompatActivity {
         });
 
 
+
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
 
@@ -76,6 +111,17 @@ public class UserProfile extends AppCompatActivity {
                 editTextusername.setText(user.getUsername());
                 editTextaddress.setText(user.getAddress());
                 editTextphoneno.setText(user.getPhone());
+                propicextension = user.getImageurlext();
+                firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                StorageReference proref = storageReference.child(firebaseUser.getUid()+"."+propicextension);
+                proref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).fit().placeholder(R.drawable.ic_baseline_account_circle_24).centerCrop().into(profilepic);
+                    }
+                });
+
+                //Picasso.get().load(user.getImageurl()).fit().placeholder(R.drawable.ic_baseline_image_24).centerCrop().into(profilepic);
             }
 
             @Override
@@ -83,6 +129,8 @@ public class UserProfile extends AppCompatActivity {
 
             }
         });
+
+
 
 
 
@@ -100,7 +148,45 @@ public class UserProfile extends AppCompatActivity {
                 hashMap.put("address",updated_address);
                 hashMap.put("phone",updated_phoneno);
                 databaseReference.updateChildren(hashMap);
-                Toast.makeText(getApplicationContext(),"User information updated successfully",Toast.LENGTH_SHORT).show();
+
+                if (picselected==0)
+                    Toast.makeText(getApplicationContext(),"User information updated successfully",Toast.LENGTH_SHORT).show();
+
+
+                try {
+                    if (picselected == 1) {
+                        final StorageReference ref = storageReference.child(firebaseUser.getUid() + "." + getfileextension(image_url));
+                        final ProgressDialog progressDialog = new ProgressDialog(UserProfile.this);
+                        progressDialog.setMessage("Uploading");
+                        progressDialog.show();
+                        ref.putFile(image_url)
+                                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                        databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+                                        HashMap<String, Object> hashMap = new HashMap<>();
+                                        hashMap.put("imageurlext", getfileextension(image_url));
+                                        databaseReference.updateChildren(hashMap);
+                                        progressDialog.dismiss();
+                                        Toast.makeText(getApplicationContext(),"User information updated successfully",Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception exception) {
+                                        // Handle unsuccessful uploads
+                                        // ...
+                                        //Toast.makeText(getApplicationContext(),exception.getMessage(),Toast.LENGTH_SHORT).show();
+                                        progressDialog.dismiss();
+                                    }
+                                });
+
+                    }
+                }catch (Exception e)
+                {
+                    //Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"User information updated successfully",Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
@@ -115,6 +201,14 @@ public class UserProfile extends AppCompatActivity {
                         editTextusername.setText(user.getUsername());
                         editTextaddress.setText(user.getAddress());
                         editTextphoneno.setText(user.getPhone());
+                        StorageReference proref = storageReference.child(firebaseUser.getUid()+"."+propicextension);
+                        proref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Picasso.get().load(uri).fit().placeholder(R.drawable.ic_baseline_account_circle_24).centerCrop().into(profilepic);
+                                image_url=null;
+                            }
+                        });
 
                         Toast.makeText(getApplicationContext(),"User information restored",Toast.LENGTH_SHORT).show();
                     }
@@ -127,6 +221,21 @@ public class UserProfile extends AppCompatActivity {
             }
         });
     }
+
+    private void OpenImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,IMAGE_REQUEST);
+    }
+
+    private String getfileextension(Uri uri)
+    {
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home)
@@ -135,5 +244,16 @@ public class UserProfile extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==IMAGE_REQUEST && resultCode==RESULT_OK && data!=null && data.getData()!=null)
+        {
+            image_url = data.getData();
+            Picasso.get().load(image_url).fit().centerCrop().into(profilepic);
+        }
     }
 }
